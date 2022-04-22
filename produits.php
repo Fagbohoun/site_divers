db833badb35255b8ae1551b476db1a9595b849cb
fagbohoun <famarcel223@gmail.com>
2022-04-22 10:17:13 +0100

    Ajout du fichier produit.css et produits.php

--- produits.php
+++ produits.php
@@ -18,31 +18,30 @@
 <body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <?php foreach( $table_general as $table ):?>
+   
 
-<div class="container" >
-            <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-12">
+<div class="container mt-3" >
+            <div class="row" <?php foreach( $table_general as $table ):?>>
+            <div class="col-lg-3 col-md-3 col-sm-12 col-12"  >
                     <div class="card">
                         <div class="card-top-img">
-                        <?php echo " <img style='width: 289px;' src='./upload/". $table['images']."'>"
-                        ;?>
+                        <?php echo " <img style='width: 305px;' src='./upload/". $table['images']."'>";?>
                         </div>
-                        <div class="card-body">
-                            <div class="card-title"><h5><?php echo $table['Names'];?></h5> </div>
+                        <div class="card-body bg-info">
+                            <div class="card-title text-secondary"><h5><?php echo $table['Names'];?></h5> </div>
                         </div>
-                        <div class="card-text">
+                        <div class="card-text ">
                            <p><?php echo $table['commentaire'];?></p> 
     </br>
-                            <p><?php echo "Prix: ". $table['price'] ."FCFA";?></p>
+                            <p class="price"><?php echo "Prix: ". $table['price'] ." FCFA";?></p>
                         </div>
-                    </div>
-                </div>
+                    </div  >
+                </div <?php endforeach;?>>
 
             </div>
                
  </div>
         
-        <?php endforeach;?>
+       
 </body>
 </html>
